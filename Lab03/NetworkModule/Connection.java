3b1d945551cd47c5f45659fc47b86b9c685fb6d8
DanielSalis <danielsalis98@gmail.com>
2022-06-18 18:22:20 -0300

    starting connection

---
+++ Lab03/NetworkModule/Connection.java
@@ -0,0 +1,34 @@
+package NetworkModule;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Connection extends Thread {
+
+    @Override
+    public void run() {
+
+        ServerSocket server;
+        try {
+            server = new ServerSocket(3002);
+            while (true) {
+                Socket socket = null;
+                try {
+                    socket = server.accept();
+                    Server s = new Server(socket);
+
+                    s.start();
+                    System.out.println("Conectou");
+                } catch (Exception ex) {
+                    socket.close();
+                    ex.printStackTrace();
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
