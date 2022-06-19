20149a4f73e19ec447747e6b5f3547db14973a32
DanielSalis <danielsalis98@gmail.com>
2022-06-19 20:41:48 -0300

    passing props to methods

--- Lab03/Chat.java
+++ Lab03/Chat.java
@@ -28,6 +28,10 @@ public class Chat extends JFrame {
     public JLabel encryptor__label;
     public JComboBox<String> encryptor__combobox;
 
+    public JPanel cypher__pannel;
+    public JLabel cypher__label;
+    public JComboBox<String> cypher__combobox;
+
     public Chat() {
         drawScreen();
         this.client = new Client();
@@ -37,9 +41,14 @@ public class Chat extends JFrame {
         textArea.setText(textArea.getText().concat(who + ":\n" + mes + "\n\n"));
     }
 
-    private void sendMessage(java.awt.event.ActionEvent evt) {
+    private void sendMessage(ActionEvent evt) {
+        System.out.println(cypher__combobox.getSelectedItem().toString());
+        System.out.println(encryptor__combobox.getSelectedItem().toString());
+        System.out.println(ip__textField.getText());
+        System.out.println(message__textField.getText());
+
         if (this.client.getSocket() == null || this.ip__textField.getText() == "") {
-            JOptionPane.showMessageDialog(null, "Make sure you filled in the ip input");
+            JOptionPane.showMessageDialog(null, "Make sure you filled in the ip input or connected");
             return;
         }
 
@@ -53,7 +62,7 @@ public class Chat extends JFrame {
         }
     }
 
-    private void connectToIp(java.awt.event.ActionEvent evt) {
+    private void connectToIp(ActionEvent evt) {
         try {
             if (this.ip__textField.getText().isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Make sure you filled in the ip input");
@@ -64,6 +73,14 @@ public class Chat extends JFrame {
         } catch (IOException ex) {
             System.out.println(ex);
         }
+    }
+
+    private void handleChangeEncrypt(ActionEvent evt){
+        System.out.println(encryptor__combobox.getSelectedItem().toString());
+    }
+
+    private void handleChangeCypher(ActionEvent evt){
+        System.out.println(cypher__combobox.getSelectedItem().toString());
     }
 
     private void drawScreen() {
@@ -102,10 +119,28 @@ public class Chat extends JFrame {
         encryptor__pannel = new JPanel();
         encryptor__label = new JLabel("Encrypt Mode");
         encryptor__combobox = new JComboBox<>();
-        encryptor__combobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ECB", "CBC", "Nenhum" }));
+        encryptor__combobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "SDES", "RC4"}));
+        encryptor__combobox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                handleChangeEncrypt(evt);
+            }
+        });
         encryptor__pannel.add(encryptor__label);
         encryptor__pannel.add(encryptor__combobox);
         encryptor__pannel.setBackground(Color.PINK);
+
+        cypher__pannel = new JPanel();
+        cypher__label = new JLabel("Cypher Mode");
+        cypher__combobox = new JComboBox<>();
+        cypher__combobox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ECB", "CBC"}));
+        cypher__combobox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                handleChangeCypher(evt);
+            }
+        });
+        cypher__pannel.add(cypher__label);
+        cypher__pannel.add(cypher__combobox);
+        cypher__pannel.setBackground(Color.YELLOW);
 
         // Creating the message__panel at bottom and adding components
         message__panel = new JPanel();
@@ -125,11 +160,12 @@ public class Chat extends JFrame {
         topPanel = new JPanel();
         topPanel.add(ip__pannel);
         topPanel.add(encryptor__pannel);
+        topPanel.add(cypher__pannel);
         topPanel.setBackground(Color.GRAY);
 
         bottomPanel = new JPanel();
         bottomPanel.add(message__panel);
-        bottomPanel.setBackground(Color.BLUE);
+        bottomPanel.setBackground(Color.GRAY);
 
         // Adding Components to the frame.
         frame.getContentPane().add(topPanel);
