79c81c1436915417ba1e4104e1d52eb74a9e21a2
DanielSalis <danielsalis98@gmail.com>
2022-06-19 13:41:43 -0300

    format file

--- Lab03/CryptoModule/RC4.java
+++ Lab03/CryptoModule/RC4.java
@@ -7,27 +7,27 @@ import java.util.Scanner;
 
 public class RC4 {
 	public Scanner scanner = new Scanner(System.in);
-  public String plain_text;
-  public char[] key;
-  public static int BYTES = 256;
+	public String plain_text;
+	public char[] key;
+	public static int BYTES = 256;
 
-  public void run(){
-    plain_text = scanner.nextLine();
-    key = scanner.nextLine().toCharArray();
+	public void run() {
+		plain_text = scanner.nextLine();
+		key = scanner.nextLine().toCharArray();
 
-    cifrar(plain_text, key);
-  }
+		cifrar(plain_text, key);
+	}
 
 	public static char[] getS() {
 		char[] aux = new char[BYTES];
 
 		for (int i = 0; i < aux.length; i++)
-			aux[i] = (char)i;
+			aux[i] = (char) i;
 
 		return aux;
 	}
 
-	public static char[] getT (char[] K) {
+	public static char[] getT(char[] K) {
 		char[] aux = new char[BYTES];
 		int j = 0;
 
@@ -41,7 +41,7 @@ public class RC4 {
 		return aux;
 	}
 
-	public static char[] executeSFunction (char[] S, char[] T) {
+	public static char[] executeSFunction(char[] S, char[] T) {
 		int j = 0;
 
 		for (int i = 0; i < BYTES; i++) {
@@ -52,13 +52,13 @@ public class RC4 {
 		return S;
 	}
 
-	public static void swap (char[] S, int i, int j) {
+	public static void swap(char[] S, int i, int j) {
 		char aux = S[i];
 		S[i] = S[j];
 		S[j] = aux;
 	}
 
-	public int[] applyXor (int[] a, int[] b) {
+	public int[] applyXor(int[] a, int[] b) {
 		int[] aux = new int[a.length];
 
 		for (int i = 0; i < a.length; i++) {
@@ -71,35 +71,35 @@ public class RC4 {
 		return aux;
 	}
 
-	public int [] convertIntToBin (int num) {
-		int bin [] = new int[8];
+	public int[] convertIntToBin(int num) {
+		int bin[] = new int[8];
 		int i = 7;
 
-		while (i >- 1) {
+		while (i > -1) {
 			bin[i] = num % 2;
-			num = num/2;
+			num = num / 2;
 			i--;
 		}
 
 		return bin;
 	}
 
-	public char convetBinToChar (int[] bin) {
+	public char convetBinToChar(int[] bin) {
 		long currentAscString = 0;
 
-		for (int i = bin.length-1; i >- 1; i--) {
-			currentAscString += Math.pow(2, bin.length-1-i) * bin[i];
+		for (int i = bin.length - 1; i > -1; i--) {
+			currentAscString += Math.pow(2, bin.length - 1 - i) * bin[i];
 		}
 
 		return (char) currentAscString;
 	}
 
-	public void streamGeneration (char [] S, String entrada) {
+	public void streamGeneration(char[] S, String entrada) {
 		int i = 0, j = 0, l = 0, k, t;
 
 		Charset currentCharSet = StandardCharsets.UTF_8;
 		ByteBuffer BytesArray = currentCharSet.encode(entrada);
-		byte [] aux = BytesArray.array();
+		byte[] aux = BytesArray.array();
 
 		while (true) {
 			i = (i + 1) % BYTES;
@@ -107,25 +107,24 @@ public class RC4 {
 			swap(S, i, j);
 			t = (S[i] + S[j]) % BYTES;
 			k = S[t];
-			if (l < entrada.length()){
+			if (l < entrada.length()) {
 				int[] xorResult = this.applyXor(this.convertIntToBin(k), this.convertIntToBin((int) aux[l++]));
 				System.out.print(Integer.toHexString(this.convetBinToChar(xorResult)) + ":");
-			}
-			else
+			} else
 				break;
 		}
 	}
 
-	public void cifrar(String entrada, char[] K){
-		char [] S = getS();
-		char [] T = getT(K);
+	public void cifrar(String entrada, char[] K) {
+		char[] S = getS();
+		char[] T = getT(K);
 
 		executeSFunction(S, T);
 		this.streamGeneration(S, entrada);
 	}
 
-	public static void main (String[] args){
-    RC4 program = new RC4();
-    program.run();
+	public static void main(String[] args) {
+		RC4 program = new RC4();
+		program.run();
 	}
 }
