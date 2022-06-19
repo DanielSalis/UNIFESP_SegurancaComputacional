63724869171cce169cc916911db7a0b736900bd3
DanielSalis <danielsalis98@gmail.com>
2022-06-19 17:58:13 -0300

    creating client class

---
+++ Lab03/NetworkModule/Client.java
@@ -0,0 +1,32 @@
+package NetworkModule;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    private OutputStream outputStream;
+    private Writer writer;
+    private BufferedWriter bufferWriter;
+
+    public Client() {
+        this.socket = null;
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public void conectar(String ipAdress) throws IOException {
+        socket = new Socket(ipAdress, 3000);
+        outputStream = socket.getOutputStream();
+        writer = new OutputStreamWriter(outputStream);
+        bufferWriter = new BufferedWriter(writer);
+        bufferWriter.flush();
+    }
+
+    public void enviarMensagem(String msg) throws IOException {
+        bufferWriter.write(msg + "\r\n");
+        bufferWriter.flush();
+    }
+}
