404e0c1b5827c4c316a97212c60231e429de76ca
DanielSalis <danielsalis98@gmail.com>
2022-07-10 17:37:22 -0300

    add client

--- Lab04/modules/NetworkModule/Client.java
+++ Lab04/modules/NetworkModule/Client.java
@@ -2,19 +2,27 @@ package modules.NetworkModule;
 
 import java.io.*;
 import java.net.Socket;
+import modules.KeyChanger.*;
 
 public class Client {
     private Socket socket;
     private OutputStream outputStream;
     private Writer writer;
     private BufferedWriter bufferWriter;
+    private final DH dh;
 
     public Client() {
         this.socket = null;
+        this.dh = new DH(4, 0);
     }
 
     public Socket getSocket() {
         return this.socket;
+    }
+
+    public DH getDh()
+    {
+        return this.dh;
     }
 
     public void conectar(String ipAdress) throws IOException {
@@ -23,6 +31,18 @@ public class Client {
         writer = new OutputStreamWriter(outputStream);
         bufferWriter = new BufferedWriter(writer);
         bufferWriter.flush();
+        this.dh.setX(this.dh.gerarX());
+        System.out.println("Set x: "+this.dh.getX());
+    }
+
+    public void enviarY(){
+        this.dh.setX((int)(Math.random() * (this.dh.getQ())));
+        this.dh.generateY();
+        try {
+            enviarMensagem("Header|Y:"+this.dh.getY()+"|Header");
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
     }
 
     public void enviarMensagem(String msg) throws IOException {
